8c2a8e0fd0353b7eccb0d8209b533f0eacdc0f37
Diego Soto <sot22737@uvg.edu.gt>
2023-03-03 08:33:32 -0600

    Factory

--- FactoryHashmap.java
+++ FactoryHashmap.java
@@ -1,3 +1,28 @@
+import java.util.*;
+
 public class FactoryHashmap {
+    Map<String, ArrayList<String>> hash;
+
+    public Map<String, ArrayList<String>> getInstance(int tipo){
+
     
+        switch (tipo) {
+            case 1:
+            hash = new HashMap<String, ArrayList<String>>();
+            break;
+
+            case 2:
+            hash = new LinkedHashMap<String, ArrayList<String>>();
+            break;
+
+            case 3:
+            hash = new TreeMap<String, ArrayList<String>>();
+            break;
+
+            default:
+            break;
+        }
+        return hash;
+
+    } 
 }
